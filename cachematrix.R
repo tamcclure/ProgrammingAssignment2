a843c29e866284842adb08aba400ca24bc2ecba9
Tom <tamcclure@zoominternet.net>
2015-04-20 20:43:55 -0400

    Created two functions to solve an Inverse Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a Cache for solving an inverse matrix
+##  Uses two functions: makeCacheMatrix as the list and CacheSolve for final solution
 
-## Write a short comment describing this function
+## It creates a list to allow the use of the stored value of the variable s, 
+## which is used to solve the inverse matrix
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix){
+  s<-NULL
+  set<-function(y){
+    x<<-y
+    s<<-NULL    
+  }
+  get<-function()x
+  setinverse<-function(solve)s<<-solve
+  getinverse<-function()s
+  list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## It finds the stored value for the variable s if it has been stored;
+## Otherwise cacheSolve will create the value for the variable s and store it in the cache
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x,...){
+  if(!is.null(s)){
+    return(s)
+  }
+  data<-x$get()
+  s<-solve(data,...)
+  x$setinverse(s)
+  s
 }
